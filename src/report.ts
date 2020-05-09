095643c7f8da2d4fc20d63cd9d898e0821cb7489
aki <lala.akira@gmail.com>
2020-05-10 08:47:09 +0900

    1.0.2

--- src/report.ts
+++ src/report.ts
@@ -1,36 +1,7 @@
 import * as core from '@actions/core'
 import * as github from '@actions/github'
+import replaceComment from '@aki77/actions-replace-comment'
 import markdownTable from 'markdown-table'
-
-const HEADER = '## Simplecov Report'
-
-const issues = (): github.GitHub['issues'] => {
-  return new github.GitHub(core.getInput('token')).issues
-}
-
-const getOldCommentIds = async (pullRequestId: number): Promise<number[]> => {
-  const {data: existingComments} = await issues().listComments({
-    owner: github.context.repo.owner,
-    repo: github.context.repo.repo,
-    // eslint-disable-next-line @typescript-eslint/camelcase
-    issue_number: pullRequestId
-  })
-
-  return existingComments.filter(({body}) => body.startsWith(HEADER)).map(({id}) => id)
-}
-
-const deleteOldComments = async (pullRequestId: number): Promise<void> => {
-  const ids = await getOldCommentIds(pullRequestId)
-
-  for (const id of ids) {
-    issues().deleteComment({
-      owner: github.context.repo.owner,
-      repo: github.context.repo.repo,
-      // eslint-disable-next-line @typescript-eslint/camelcase
-      comment_id: id
-    })
-  }
-}
 
 export async function report(coveredPercent: number, failedThreshold: number): Promise<void> {
   const summaryTable = markdownTable([
@@ -43,14 +14,13 @@ export async function report(coveredPercent: number, failedThreshold: number): P
     throw new Error('Cannot find the PR id.')
   }
 
-  deleteOldComments(pullRequestId)
-
-  await issues().createComment({
+  await replaceComment({
+    token: core.getInput('token', {required: true}),
     owner: github.context.repo.owner,
     repo: github.context.repo.repo,
     // eslint-disable-next-line @typescript-eslint/camelcase
     issue_number: pullRequestId,
-    body: `${HEADER}
+    body: `## Simplecov Report
 ${summaryTable}
 `
   })
